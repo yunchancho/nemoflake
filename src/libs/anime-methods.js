9da62f7c61cabe9e450e828243047cfd092f7cce
Vine Brancho <vinebrancho@gmail.com>
2017-08-18 18:58:24 +0900

    update intro animation of flakes

--- src/libs/anime-methods.js
+++ src/libs/anime-methods.js
@@ -12,44 +12,45 @@ export function enterSample1(flake, metadata = {}, finishCallback) {
       return anime.random(-500, 500);
     },
     scale: function(el, i, l) {
-      return anime.random(0.7, 2.0);
-    },
-    rotate: function(el, i) {
-      return anime.random(0, 0);
+      return anime.random(0.5, 2.0);
     },
     duration: function() { return anime.random(1200, 1800); },
-    duration: function() { return anime.random(800, 1600); },
     delay: function() { return 500 + anime.random(0, 1000); },
-    direction: metadata.direction | 'alternate', 
-    loop: metadata.loop | 1
+    elasticity: function(target, index, totalTargets) {
+      // Elasticity multiplied by every div index, in descending order
+      return 100 + ((totalTargets - index) * 150);
+    },
+    direction: 'alternate', 
+    loop: metadata.loop | 1,
+    complete: finishCallback
   });
 }
 
 export function enterSample2(flake, metadata = {}, finishCallback) {
-  anime.timeline({ 
-    direction: metadata.direction | 'alternate', 
-    loop: metadata.loop | 1
+  let timeline = anime.timeline({ 
+    loop: true,
   })
-  .add({
-    targets: flake,
-    translateX: [ { value: 80 }, { value: 160 }, { value: 250 } ],
-    translateY: [ { value: 30 }, { value: 60 }, { value: 60 } ],
-    duration: 3000
-  })
-  .add({
-    targets: flake,
-    translateX: [ { value: 80 }, { value: 160 }, { value: 250 } ],
-    translateY: [ { value: 30 }, { value: -30 }, { value: -30 } ],
-    duration: 3000,
-    offset: 200
-  })
-  .add({
-    targets: flake,
-    translateX: [ { value: 80 }, { value: 250 } ],
-    translateY: [ { value: -60 }, { value: -30 }, { value: -30 } ],
-    duration: 3000,
-    offset: 400
-  });
+
+  for (let item of flake) {
+    timeline.add({
+      targets: item,
+      translateY: [
+        { value: -100, elasticity: 250 },
+        { value: 0 }
+      ],
+      scaleX: [
+        { value: 0.5, duration: 300, easing: 'easeOutExpo' },
+        { value: 1, duration: 500, easing: 'easeOutExpo' }
+      ],
+      scaleY: [
+        { value: 0.5, duration: 300, easing: 'easeOutExpo' },
+        { value: 1, duration: 500, easing: 'easeOutExpo' }
+      ],
+      direction: 'alternate',
+      duration: 3000,
+      offest: '-=1000'
+    });
+  }
 }
 
 export function enterSample3(flake, metadata = {}, finishCallback) {
